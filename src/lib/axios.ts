76848566c8619735d8475f765a36ffee12524c19
ZawHlaingPhyoTsuki <tro2233zhp@gmail.com>
2025-02-28 13:06:51 +0700

    axios base url updated 3

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://moonappleonlineshop.vercel.app/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
